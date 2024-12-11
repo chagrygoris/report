170942c2a0e0c98350ac5b87f798f576abcb9dde
Grigory <grigory.chaykovsky@gmail.com>
2024-12-11 23:44:13 +0300

    fk&tests&fixed

--- alembic/models.py
+++ alembic/models.py
@@ -1,8 +1,15 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, String, Integer, MetaData, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
 
 Base = declarative_base()
+Base.metadata = MetaData(naming_convention=convention)
 metadata = Base.metadata
 
 class User(Base):
@@ -17,4 +24,4 @@ class Playlist(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     genre = Column(String(50))
-    creator = Column(Integer, ForeignKey('t_users.id'))
+    creator = Column(Integer, ForeignKey(User.id))
